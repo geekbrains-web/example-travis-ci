9d6dd8dbc82f0bd467a47ccb23c3634083254dac
Oleg Shikov <ol.shikov@yandex.ru>
2020-03-26 20:46:49 +0300

    Fix code style

--- src/isAdult.test.js
+++ src/isAdult.test.js
@@ -1,14 +1,14 @@
-const isAdult = require('./isAdult');
+const isAdult = require("./isAdult");
 
 test("Is age > 18 is true", () => {
-  expect(isAdult(35)).toBe(true);
+	expect(isAdult(35)).toBe(true);
 });
 
 test("Is age < 18 is false", () => {
-  expect(isAdult(14)).toBe(false);
+	expect(isAdult(14)).toBe(false);
 });
 
 
 test("Is age = 18 is false", () => {
-  expect(isAdult(18)).toBe(true);
+	expect(isAdult(18)).toBe(true);
 });
